41fbb861f210ca20612a84a5a4d52f75b8b2069e
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-05-21 11:44:23 -0400

    Home screen make text stand out

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -7,16 +7,18 @@ const Home = () => {
     document.body.classList.add('h-full');
     document.body.classList.add('m-0');
     document.body.classList.add('p-0');
+    
   });
 
   return (
-    <div className="container flex h-full bg-[url('https://i.unu.edu/media/ourworld.unu.edu-en/article/8602/e-waste.jpg')] opacity-60 bg-no-repeat bg-cover bg-center">
-      <div className="w-full flex-col absolute text-center justify-center mt-72 text-white opacity-100">
-        <h1 className="text-6xl">Hermitrade</h1>
-        <h4 className="mt-8">Technology owes ecology an apology</h4>
+    <div>
+      <div className="absolute w-full h-full inset-0 bg-[url('https://i.unu.edu/media/ourworld.unu.edu-en/article/8602/e-waste.jpg')] opacity-60 bg-no-repeat bg-cover bg-center -z-20"/>
+      <div className="inset-0 container flex h-full z-10 opacity-100 items-center justify-center mt-16">
+        <div className="flex-col absolute text-center justify-center mt-96 text-white bg-black bg-opacity-40 shadow-[0_0_20px_30px_rgba(0,0,0,0.4)]">
+          <h1 className="text-6xl">Hermitrade</h1>
+          <h4 className="mt-8">Technology owes ecology an apology</h4>
+        </div>
       </div>
-      
-      
     </div>
   );
 };
