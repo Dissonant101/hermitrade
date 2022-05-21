89ef1c448d3dc912132ba7371329b23134ff94d8
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-05-21 09:01:38 -0400

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,46 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useState, useContext } from "react";
+import { HermitradeContext } from "../context/HermitradeContext";
 
 const Navbar = () => {
+  const { currentAccount, connectWallet } = useContext(HermitradeContext)
   return (
-    <div className="navigation">
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <div className="container">
-          <NavLink className="navbar-brand" to="/">
-            Hermit Trade
-          </NavLink>
-          <div>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/">
+    <div className="navigation w-full font-sans text-xl opacity-100">
+      <nav className="navbar navbar-expand navbar-dark bg-dark w-full">
+        <div className="container w-full justify-between bg-blue-400">
+          <ul className="navbar-nav ml-auto flex flex-wrap items-start justify-start px-2 py-3 align-middle">
+            <li className="nav-item mx-3">
+              <div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
+                <NavLink className="nav-link p-3" to="/">
                   Home
                   <span className="sr-only">(current)</span>
                 </NavLink>
-              </li>
-              <li className="nav-item">
+              </div>
+            </li>
+            <li className="nav-item mx-3;">
+              <div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
                 <NavLink className="nav-link" to="/about">
                   About
                 </NavLink>
-              </li>
-              <li className="nav-item">
+              </div>
+            </li>
+            <li className="nav-item mx-3">
+              <div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
                 <NavLink className="nav-link" to="/market">
                   Marketplace
                 </NavLink>
-              </li>
-              <li className="nav-item">
+              </div>
+            </li>
+            <li className="nav-item mx-3">
+              <div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
                 <NavLink className="nav-link" to="/post">
                   Post Deals
                 </NavLink>
-              </li>
-            </ul>
-          </div>
+              </div>
+            </li>
+          {currentAccount.length == 0 ? (
+          <button type="button" onClick={connectWallet} className=" inset-y-0 bg-[#e29608] px-3 py-2 rounded-full float-right mr-2 ml-auto duration-300 hover:bg-[#bd7e0a]">Connect wallet</button>) : (<div className="m-1 p-1">Wallet connected!</div>)}
+          </ul>
         </div>
       </nav>
     </div>
