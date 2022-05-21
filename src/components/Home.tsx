7b4f312b1a0b8a4b09a0c7e57c5196bd948023bd
JaryJay <jay.jrjren@gmail.com>
2022-05-21 00:33:19 -0400

    Connect wallet button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { HermitradeContext } from "../context/HermitradeContext";
 
 const Home = () => {
+  const { currentAccount, connectWallet } = useContext(HermitradeContext)
   return (
-    <div className="w-full flex">
+    <div className="w-full">
       <h1>Welcome to HermitTrade.</h1>
+      <br />
+      {currentAccount.length == 0 ? (
+        <button type="button" onClick={connectWallet} className="w-20 bg-[#e29608] p-3 m-1 rounded-full">Connect wallet</button>
+      ) : (<div className="m-1 p-1">Wallet connected!</div>)}
     </div>
   );
 };
