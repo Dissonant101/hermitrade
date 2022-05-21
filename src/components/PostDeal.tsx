eaec71a9d24969434d8f8355e45cd17e1a785f25
Dissonant101 <kak100kak@gmail.com>
2022-05-21 09:31:07 -0400

    Updated post deal input fields

--- src/components/PostDeal.tsx
+++ src/components/PostDeal.tsx
@@ -8,17 +8,21 @@ const PostDeal = () => {
       <br />
       <input type="email" id="email" name="email" />
       <br />
-      <label htmlFor="address">Wallet Address:</label>
-      <br />
-      <input type="text" id="address" name="address" />
-      <br />
       <label htmlFor="item">Item:</label>
       <br />
       <input type="text" id="item" name="item" />
       <br />
+      <label htmlFor="description">Description:</label>
+      <br />
+      <input type="text" id="description" name="description" />
+      <br />
       <label htmlFor="price">Price:</label>
       <br />
       <input type="text" id="price" name="price" />
+      <br />
+      <label htmlFor="imageurl">Image URL:</label>
+      <br />
+      <input type="text" id="imageurl" name="imageurl" />
       <br />
       <input type="submit" value="Submit" />
       <br />
