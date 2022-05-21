ccf9c2199c11244af51383fce01ff48bb553eba7
Dissonant101 <kak100kak@gmail.com>
2022-05-21 01:32:04 -0400

    Very rough input fields for posting

--- client/src/components/PostDeal.tsx
+++ client/src/components/PostDeal.tsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
 
 const PostDeal = () => {
-  return <h1>This is the page for posting deals.</h1>;
+  return (
+    <form>
+      <br />
+      <label htmlFor="email">Email:</label>
+      <br />
+      <input type="email" id="email" name="email" />
+      <br />
+      <label htmlFor="address">Wallet Address:</label>
+      <br />
+      <input type="text" id="address" name="address" />
+      <br />
+      <label htmlFor="item">Item:</label>
+      <br />
+      <input type="text" id="item" name="item" />
+      <br />
+      <label htmlFor="price">Price:</label>
+      <br />
+      <input type="text" id="price" name="price" />
+      <br />
+      <input type="submit" value="Submit" />
+      <br />
+    </form>
+  );
 };
 
 export default PostDeal;
