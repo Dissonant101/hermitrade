7bdee25145607ccfdac222cf3ad4652d7b52e46d
JaryJay <jay.jrjren@gmail.com>
2022-05-20 23:46:49 -0400

    Create HermitradeContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 import { Market, Navbar, PostDeal, Welcome } from './components'
 import './App.css'
 
-interface Window {
-  ethereum: any
-}
 
 function App() {
-
   return (
     <h1 className="text-3xl font-bold underline">
       <div>
--- client/src/context/HermitradeContext.tsx
+++ client/src/context/HermitradeContext.tsx
@@ -1,5 +1,48 @@
-import React from 'react';
-
-export const TransactionContext = React.createContext('');
+import { createContext, useState } from 'react';
+import { ethers } from "ethers";
 
 const { ethereum } = window;
+
+const HermitradeContext = createContext({});
+
+export const HermitradeContextProvider = ({ children }: { children: any }) => {
+	const [currentAccount, setCurrentAccount] = useState("");
+
+
+	const connectWallet = async () => {
+		try {
+			if (!ethereum) return alert("Please install MetaMask.");
+
+			const accounts: string[] = await ethereum.request({ method: "eth_requestAccounts", });
+			console.log(accounts[0]);
+			setCurrentAccount(accounts[0]);
+			console.log(currentAccount);
+		} catch (error) {
+			console.log(error);
+
+			throw new Error("No ethereum object");
+		}
+	};
+
+	const sendMoney = async (addressTo: string, amount: string) => {
+		await ethereum.request({
+			method: "eth_sendTransaction",
+			params: [{
+				from: currentAccount,
+				to: addressTo,
+				gas: "0x5208",
+				value: ethers.utils.parseEther(amount)._hex,
+			}],
+		});
+	}
+
+	return (
+		<HermitradeContext.Provider value={{
+			currentAccount
+		}}>
+			{children}
+		</HermitradeContext.Provider>
+	)
+}
+
+export default HermitradeContextProvider;
