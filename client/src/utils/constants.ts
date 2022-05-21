7eb606aca519187dc6d5747c540c8cbb3c949241
JaryJay <jay.jrjren@gmail.com>
2022-05-21 07:23:59 -0400

    Copy contract abi to client folder

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,4 @@
-export const contractAddress = "0xa62D78181053693a591c5234Bb2b01e0fAC40e28";
+import abi from './Deals.json';
+
+export const contractAbi = abi.abi; // Hover to see object properties
+export const contractAddress: string = "0xa62D78181053693a591c5234Bb2b01e0fAC40e28";
