1e541490c3dba52c48e4858fcdefa0ecfaa0c0d7
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2022-05-21 13:25:34 -0400

    Gradient on about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import { HermitradeContext } from "../context/HermitradeContext";
 
 const About = () => {
-	return <h1>This is the about page.</h1>;
+	return (
+		<div className="w-full h-full about-bg">
+			<div className="flex-col text-center justify-center p-80 text-white ">
+				<h1 className="text-6xl">About</h1>
+				<h4 className="mt-8">What is Hermitrade?</h4>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,20 +3,29 @@ import { HermitradeContext } from "../context/HermitradeContext";
 
 const Home = () => {
 
+  const backgroundImageData = useState({
+    currentImage: 0,
+    imageURLs: ['url(https://i.unu.edu/media/ourworld.unu.edu-en/article/8602/e-waste.jpg)',
+      'url(https://interconnection.org/wp-content/uploads/2019/05/scrapyard-2441432_1920.jpg)',
+      'url(http://prod-upp-image-read.ft.com/afc73936-3287-11ea-a329-0bcf87a328f2)'],
+    timeSinceLastSwitch: 0,
+  });
+
   useEffect(() => {
     document.body.classList.add('h-full');
     document.body.classList.add('m-0');
     document.body.classList.add('p-0');
-    
+
   });
 
   return (
     <div>
-      <div className="absolute w-full h-full inset-0 bg-[url('https://i.unu.edu/media/ourworld.unu.edu-en/article/8602/e-waste.jpg')] opacity-60 bg-no-repeat bg-cover bg-center -z-20"/>
-      <div className="inset-0 container flex h-full z-10 opacity-100 items-center justify-center mt-16">
-        <div className="flex-col absolute text-center justify-center mt-96 text-white bg-black bg-opacity-40 shadow-[0_0_20px_30px_rgba(0,0,0,0.4)]">
-          <h1 className="text-6xl">Hermitrade</h1>
-          <h4 className="mt-8">Technology owes ecology an apology</h4>
+      <div className="w-full h-full bg-[url('https://i.unu.edu/media/ourworld.unu.edu-en/article/8602/e-waste.jpg')] opacity-60 bg-no-repeat bg-cover bg-center">
+        <div className="flex h-full opacity-100 items-center justify-center pt-16">
+          <div className="flex-col text-center justify-center m-60 text-white bg-black bg-opacity-40 shadow-[0_0_20px_40px_rgba(0,0,0,0.4)]">
+            <h1 className="text-6xl">Hermitrade</h1>
+            <h4 className="mt-8">Technology owes ecology an apology</h4>
+          </div>
         </div>
       </div>
     </div>
