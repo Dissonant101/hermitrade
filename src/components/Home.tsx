f38ed5e1ca9e09e22bb38271e137be63600b5ec5
JaryJay <jay.jrjren@gmail.com>
2022-05-21 17:06:32 -0400

    Update home and navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,25 +3,37 @@ import { HermitradeContext } from "../context/HermitradeContext";
 
 const Home = () => {
 
-  useEffect(() => {
-    document.body.classList.add('h-full');
-    document.body.classList.add('m-0');
-    document.body.classList.add('p-0');
+	const backgroundImageData = useState({
+		currentImage: 0,
+		imageURLs: ['url(https://i.unu.edu/media/ourworld.unu.edu-en/article/8602/e-waste.jpg)',
+			'url(https://interconnection.org/wp-content/uploads/2019/05/scrapyard-2441432_1920.jpg)',
+			'url(http://prod-upp-image-read.ft.com/afc73936-3287-11ea-a329-0bcf87a328f2)'],
+		timeSinceLastSwitch: 0,
+	});
 
-  });
+	useEffect(() => {
+		document.body.classList.add('h-full');
+		document.body.classList.add('m-0');
+		document.body.classList.add('p-0');
+	});
 
-  return (
-    <div>
-      <div className="w-full h-full home-bg">
-        <div className="flex h-full opacity-100 items-center justify-center pt-16">
-          <div className="flex-col text-center justify-center m-60 text-white">
-            <h1 className="text-6xl">Hermitrade</h1>
-            <h4 className="mt-8">For <strong>all</strong> your electronic needs.</h4>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="w-full h-full home-bg text-center text-white">
+				<div className="opacity-100 items-center justify-center sm:py-6 md:py-12 py-3">
+					<div className="m-60">
+						<h1 className="text-6xl">Hermitrade</h1>
+						<h4 className="mt-8">For <strong>all</strong> your electronic needs.</h4>
+					</div>
+				</div>
+				<div className="opacity-100 items-center justify-center sm:py-6 md:py-12 py-3">
+					<h1 className="text-4xl">
+						Buy & Sell Used Electronics
+					</h1>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -3,61 +3,61 @@ import { Deal } from "../@types/Deal";
 import { HermitradeContext } from "../context/HermitradeContext";
 
 const DealCard = ({ dealInfo }: { dealInfo: Deal }) => {
-  const { sendMoney } = useContext(HermitradeContext);
-  console.log(dealInfo.timestamp);
-  console.log(new Date(dealInfo.timestamp * 1000).toISOString().substring(0, 10));
-  return (
-    <div className="m-1 p-1 border-2 border-slate-400 shadow-lg rounded-xl">
-      <img src={dealInfo.imageUrl} alt="Picture" className="" />
-      <p>
-        <b>Email:</b> {dealInfo.email}
-        <br />
-        <b>Address:</b> {dealInfo.address}
-        <br />
-        <b>Price:</b> {dealInfo.price.toString()} ETH
-        <br />
-        <b>Item:</b> {dealInfo.item}
-        <br />
-        <b>Description:</b> <br /> {dealInfo.description}
-        <br />
-        <b>Date posted:</b> {new Date(dealInfo.timestamp * 1000).toISOString().substring(0, 10)}
-      </p>
-      <button onClick={() => {
-        sendMoney(dealInfo);
+	const { sendMoney } = useContext(HermitradeContext);
+	console.log(dealInfo.timestamp);
+	console.log(new Date(dealInfo.timestamp * 1000).toISOString().substring(0, 10));
+	return (
+		<div className="m-1 p-1 border-2 border-slate-400 shadow-lg rounded-xl">
+			<img src={dealInfo.imageUrl} alt="Picture" className="" />
+			<p>
+				<b>Email:</b> {dealInfo.email}
+				<br />
+				<b>Address:</b> {dealInfo.address}
+				<br />
+				<b>Price:</b> {dealInfo.price.toString()} ETH
+				<br />
+				<b>Item:</b> {dealInfo.item}
+				<br />
+				<b>Description:</b> <br /> {dealInfo.description}
+				<br />
+				<b>Date posted:</b> {new Date(dealInfo.timestamp * 1000).toISOString().substring(0, 10)}
+			</p>
+			<button onClick={() => {
+				sendMoney(dealInfo);
 
-      }}
-        className="hover:cursor-pointer hover:bg-slate-600 hover:text-gray-200 duration-150 rounded-md px-1.5 py-0.5">
-        Purchase
-      </button>
-    </div>
-  );
+			}}
+				className="hover:cursor-pointer hover:bg-slate-600 hover:text-gray-200 duration-150 rounded-md px-1.5 py-0.5">
+				Purchase
+			</button>
+		</div>
+	);
 };
 
 const Market = () => {
 
-  const {
-    currentAccount,
-    deals,
-  }: {
-    currentAccount: string;
-    deals: Deal[];
-  } = useContext(HermitradeContext);
-  // getDeals().then(() => console.log("Finished fetching deals"));
+	const {
+		currentAccount,
+		deals,
+	}: {
+		currentAccount: string;
+		deals: Deal[];
+	} = useContext(HermitradeContext);
+	// getDeals().then(() => console.log("Finished fetching deals"));
 
-  return (
-    <div>
-      <h1>This is the market page.</h1>
-      {currentAccount.length == 0 ? (
-        <p>Connect your wallet.</p>
-      ) : (
-        <div className="grid grid-cols-3">
-          {deals.reverse().map((deal: Deal) => (
-            <DealCard dealInfo={deal} key={deal.timestamp + deal.description} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<h1>This is the market page.</h1>
+			{currentAccount.length == 0 ? (
+				<p>Connect your wallet.</p>
+			) : (
+				<div className="grid grid-cols-3">
+					{deals.reverse().map((deal: Deal) => (
+						<DealCard dealInfo={deal} key={deal.timestamp + deal.description} />
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Market;
