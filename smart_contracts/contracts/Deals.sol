d86f72276488b48326d0674f162bb21749b6e06c
JaryJay <jay.jrjren@gmail.com>
2022-05-21 13:26:09 -0400

    Puchase deals and remove them

--- smart_contracts/contracts/Deals.sol
+++ smart_contracts/contracts/Deals.sol
@@ -2,19 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract Deals {
-    event DealEvent(
-        address from,
-        uint256 timestamp,
-        uint256 price,
-        string email,
-        string item,
-        string description,
-        string imageUrl
-    );
-
     struct DealStruct {
         address from;
         uint256 timestamp;
+        uint256 id;
         uint256 price;
         string email;
         string item;
@@ -22,8 +13,9 @@ contract Deals {
         string imageUrl;
     }
 
-    uint256 dealCount = 0;
-    DealStruct[] unboughtDeals;
+    uint256 dealCount = 0; // Used as the deal id
+    mapping(uint256 => DealStruct) public unboughtDeals;
+    uint256[] public dealIds;
 
     function postDeal(
         uint256 price,
@@ -32,34 +24,44 @@ contract Deals {
         string memory description,
         string memory imageUrl
     ) public {
-        unboughtDeals.push(
-            DealStruct(
-                msg.sender,
-                block.timestamp,
-                price,
-                email,
-                item,
-                description,
-                imageUrl
-            )
-        );
-        dealCount++;
-        emit DealEvent(
+        unboughtDeals[dealCount] = DealStruct(
             msg.sender,
             block.timestamp,
+            dealCount,
             price,
             email,
             item,
             description,
             imageUrl
         );
+        dealIds.push(dealCount);
+        dealCount++;
+    }
+
+    function removeDeal(uint256 id) public {
+        delete unboughtDeals[id];
+        for (uint32 i = 0; i < dealIds.length; i++) {
+            if (dealIds[i] == id) {
+                dealIds[i] = dealIds[dealIds.length - 1];
+                dealIds.pop();
+                return;
+            }
+        }
+    }
+
+    function getDealIds() public view returns (uint256[] memory) {
+        return dealIds;
     }
 
     function getDeals() public view returns (DealStruct[] memory) {
-        return unboughtDeals;
+        DealStruct[] memory deals = new DealStruct[](dealIds.length);
+        for (uint32 i = 0; i < dealIds.length; i++) {
+            deals[i] = unboughtDeals[dealIds[i]];
+        }
+        return deals;
     }
 
     function getNumDeals() public view returns (uint256) {
-        return dealCount;
+        return dealIds.length;
     }
 }
