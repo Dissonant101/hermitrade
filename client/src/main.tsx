750677ccfc1212a1e58657c7f79d05d007ac541e
Dissonant101 <kak100kak@gmail.com>
2022-05-22 01:19:20 -0400

    Update home page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home, About, Market, Navbar, PostDeal, Footer } from "./components";
 import "./index.css";
-import HermitradeContextProvider from "./context/HermitradeContext";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HermitradeContextProvider>
-    <React.StrictMode>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/market" element={<Market />} />
-          <Route path="/post" element={<PostDeal />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </React.StrictMode>
-  </HermitradeContextProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
