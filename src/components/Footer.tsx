3d9b9c8e01faf06e84c4cf6f769461964bb10d17
JaryJay <jay.jrjren@gmail.com>
2022-05-22 08:29:30 -0400

    minor visual updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="bg-cyan-800 p-12 px-60 text-center text-white">
             <div className="grid grid-cols-2 gap-4">
                 <div>
-                    <b>HermiTrade</b><br />
+                    <b>Hermitrade</b><br />
                     <a>About</a><br />
                     <a>Contact us</a>
                 </div>
