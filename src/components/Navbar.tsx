d357607c4670421d8234e571369b61e7ef8fa23f
Dissonant101 <kak100kak@gmail.com>
2022-05-22 00:03:54 -0400

    Change navbar and about page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,71 +5,69 @@ import { BsCheckLg } from "react-icons/bs";
 import Logo from "../Hermitrade Logo.svg";
 
 const Navbar = () => {
-	const { currentAccount, connectWallet, getDeals } = useContext(HermitradeContext);
-	return (
-		<div className="navigation w-full font-sans text-xl z-10 navbar-bg text-white">
-			<nav className="navbar navbar-expand navbar-dark bg-dark w-full">
-				<div className="w-full bg-blue-400">
-					<ul className="navbar-nav ml-auto flex flex-wrap items-start justify-start px-2 py-3 align-items-center">
-						<li className="ml-5">
-							<img
-								src={Logo + "#svgView(viewBox(350.0, 0.0, 610.0, 720.0))"}
-								alt="Hermitrade"
-								style={{ width: 60, height: 45 }}
-
-							/>
-						</li>
-						<li className="nav-item mx-3">
-							<div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
-								<NavLink className="nav-link p-3" to="/">
-									Home
-									<span className="sr-only">(current)</span>
-								</NavLink>
-							</div>
-						</li>
-						<li className="nav-item mx-3">
-							<div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
-								<NavLink className="nav-link p-3" to="/about">
-									About
-								</NavLink>
-							</div>
-						</li>
-						<li className="nav-item mx-3">
-							<div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
-								<NavLink className="nav-link p-3" to="/market" onClick={() => getDeals().then(() => location.reload())}>
-									Marketplace
-								</NavLink>
-							</div>
-						</li>
-						<li className="nav-item mx-3">
-							<div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
-								<NavLink className="nav-link p-3" to="/post">
-									Post Deals
-								</NavLink>
-							</div>
-						</li>
-						{currentAccount.length == 0 ? (
-							<button
-								type="button"
-								onClick={connectWallet}
-								className="inset-y-0 bg-[#e29608] px-8 py-2 rounded-full float-right mr-2 ml-auto duration-300 hover:bg-[#bd7e0a]"
-							>
-								Connect wallet
-							</button>
-						) : (
-							<div className="float-right ml-auto mr-5 mt-1">
-								<div className="float-left ml-auto mr-6 mt-1">Connected</div>
-								<div className="float-right ml-auto mr-2 rounded-full border-2 border-white p-2 bg-[#5494e3]">
-									<BsCheckLg />
-								</div>
-							</div>
-						)}
-					</ul>
-				</div>
-			</nav>
-		</div>
-	);
-
+  const { currentAccount, connectWallet, getDeals } =
+    useContext(HermitradeContext);
+  return (
+    <div className="navigation w-full font-sans text-xl z-10 navbar-bg text-white">
+      <nav className="navbar navbar-expand navbar-dark bg-dark w-full">
+        <div className="w-full bg-blue-400">
+          <ul className="navbar-nav ml-auto flex flex-wrap items-start justify-start px-2 py-3 align-items-center">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                <img
+                  src={Logo + "#svgView(viewBox(350.0, 0.0, 610.0, 720.0))"}
+                  alt="Hermitrade"
+                  style={{ width: 60, height: 45, marginLeft: 15 }}
+                />
+                <span className="sr-only">(current)</span>
+              </NavLink>
+            </li>
+            <li className="nav-item mx-3">
+              <div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
+                <NavLink className="nav-link p-3" to="/about">
+                  About
+                </NavLink>
+              </div>
+            </li>
+            <li className="nav-item mx-3">
+              <div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
+                <NavLink
+                  className="nav-link p-3"
+                  to="/market"
+                  onClick={() => getDeals().then(() => location.reload())}
+                >
+                  Marketplace
+                </NavLink>
+              </div>
+            </li>
+            <li className="nav-item mx-3">
+              <div className="duration-300 hover:bg-blue-500 rounded-md px-2 py-2">
+                <NavLink className="nav-link p-3" to="/post">
+                  Post Deals
+                </NavLink>
+              </div>
+            </li>
+            {currentAccount.length == 0 ? (
+              <button
+                type="button"
+                onClick={connectWallet}
+                className="inset-y-0 bg-[#e29608] px-8 py-2 rounded-full float-right mr-2 ml-auto duration-300 hover:bg-[#bd7e0a]"
+              >
+                Connect wallet
+              </button>
+            ) : (
+              <div className="float-right ml-auto mr-5 mt-1">
+                <div className="float-left ml-auto mr-6 mt-1">Connected</div>
+                <div className="float-right ml-auto mr-2 rounded-full border-2 border-white p-2 bg-[#5494e3]">
+                  <BsCheckLg />
+                </div>
+              </div>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/PostDeal.tsx
+++ src/components/PostDeal.tsx
@@ -3,75 +3,119 @@ import { useNavigate } from "react-router-dom";
 import { HermitradeContext } from "../context/HermitradeContext";
 
 interface FormData {
-	email: string;
-	item: string;
-	description: string;
-	price: string;
-	url: string;
+  email: string;
+  item: string;
+  description: string;
+  price: string;
+  url: string;
 }
 
 const DealForm = () => {
-	const [formData, setFormData] = useState({
-		email: "",
-		item: "",
-		description: "",
-		price: "",
-		url: "",
-	} as FormData);
-	const { postDeals, getDeals } = useContext(HermitradeContext);
+  const [formData, setFormData] = useState({
+    email: "",
+    item: "",
+    description: "",
+    price: "",
+    url: "",
+  } as FormData);
+  const { postDeals, getDeals } = useContext(HermitradeContext);
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleSubmit = () => {
-		navigate('/market');
-	}
+  const handleSubmit = () => {
+    navigate("/market");
+  };
 
-	return (
-		<div>
-			<div className="rounded-lg post-deals-container">
-				<input type="text" placeholder="Email" className="glass-morphism" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
-				<input type="text" placeholder="Item Name" className="glass-morphism" onChange={(e) => setFormData({ ...formData, item: e.target.value })} />
-				<input type="text" placeholder="Description" className="glass-morphism" onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
-				<input type="number" placeholder="Price" className="glass-morphism" step="0.01" onChange={(e) => setFormData({ ...formData, price: e.target.value })} />
-				<input type="text" placeholder="Image URL" className="glass-morphism" onChange={(e) => setFormData({ ...formData, url: e.target.value })} />
-				<button className="rounded-full py-2 m-8 w-96 bg-[#e29608] hover:bg-[#bd7e0a] duration-200"
-					onClick={() => {
-						if (formData.email != "" && formData.item != "" && formData.description != "" && formData.price != "" && formData.url != "") {
-							postDeals(
-								formData.email,
-								formData.item,
-								formData.description,
-								formData.price,
-								formData.url
-							);
-							getDeals();
-							handleSubmit();
-						}
-					}}>
-					Submit
-				</button>
-				<br />
-			</div >
-		</div>
-	);
+  return (
+    <div>
+      <div className="rounded-lg post-deals-container">
+        <input
+          type="text"
+          placeholder="Email"
+          className="glass-morphism"
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Item Name"
+          className="glass-morphism"
+          onChange={(e) => setFormData({ ...formData, item: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          className="glass-morphism"
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          className="glass-morphism"
+          step="0.01"
+          onChange={(e) => setFormData({ ...formData, price: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Image URL"
+          className="glass-morphism"
+          onChange={(e) => setFormData({ ...formData, url: e.target.value })}
+        />
+        <button
+          className="rounded-full py-2 m-8 w-96 bg-[#e29608] hover:bg-[#bd7e0a] duration-200"
+          onClick={() => {
+            if (
+              formData.email != "" &&
+              formData.item != "" &&
+              formData.description != "" &&
+              formData.price != "" &&
+              formData.url != ""
+            ) {
+              postDeals(
+                formData.email,
+                formData.item,
+                formData.description,
+                formData.price,
+                formData.url
+              );
+              getDeals();
+              handleSubmit();
+            }
+          }}
+        >
+          Submit
+        </button>
+        <br />
+      </div>
+    </div>
+  );
 };
 
 const PostDeal = () => {
-	return (
-		<div>
-			<div className="p-48 sm:p-10 text-white post-deals-bg">
-				<div className="mx-40 mt-20 mb-40 grid grid-cols-2 text-2xl">
-					<div className="pr-12 pl-2">
-						<h1 className="text-5xl font-semibold">Sell your used electronics.</h1><br />
-						<p className="">Others will see your posts and contact you.</p><br />
-						<p>Fun fact: The US alone throws away cellphones with <b>$60 million</b> worth of gold/silver <b>yearly</b>.</p>
-					</div>
-					<div className="pl-12">
-						<DealForm />
-					</div>
-				</div>
-			</div></div>
-	)
+  return (
+    <div>
+      <div className="p-48 sm:p-10 text-white post-deals-bg">
+        <div className="mx-40 mt-20 mb-40 grid grid-cols-2 text-2xl">
+          <div className="pr-12 pl-2">
+            <h1 className="text-5xl font-semibold">
+              Sell your used electronics.
+            </h1>
+            <br />
+            <p className="">Others will see your posts and contact you.</p>
+            <br />
+            <p>
+              Fun fact: The US alone throws away cellphones with{" "}
+              <b>$60 million</b> worth of gold/silver <b>yearly</b>.
+            </p>
+          </div>
+          <div className="pl-12">
+            <DealForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostDeal;
